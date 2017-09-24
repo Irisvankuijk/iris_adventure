fd48f740d10ad7890e9e0aa5e299510dc752d0a4
Irisvankuijk <32243203+Irisvankuijk@users.noreply.github.com>
2017-09-24 17:17:54 +0200

    Initial commit

---
+++ game.py
@@ -0,0 +1,15 @@
+print "Hi! Welcome to game.py"
+name = raw_input("What's your name? ")
+print "Hi Mr/Miss: " + name
+world_size = 5
+current_position = 0
+
+print "Your current position is " + str(current_position)
+
+command = raw_input("What do you want to do? ")
+if command == 'f':
+    next_position = current_position+1
+    current_position=next_position
+    print "Your current position is " + str(current_position)
+
+print "The End."
